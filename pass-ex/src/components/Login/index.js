3d8de712ae6ca2bdd62af88b8b6d119746951dac
Kaylin Boyle <kaylinnboyle@gmail.com>
2022-02-18 22:40:30 -0800

    login wireframe added

--- pass-ex/src/components/Login/index.js
+++ pass-ex/src/components/Login/index.js
@@ -4,9 +4,40 @@ import { Link } from "react-router-dom";
 
 function Login() {
     return (
+
         <section className="login">
+            <form class="login-form">
+                <div>
+                    <label for="email-login">email:</label>
+                    <input type="text" id="email-login" />
+                </div>
+                <div>
+                    <label for="password-login">password:</label>
+                    <input type="password" id="password-login" />
+                </div>
+                <div>
+                    <button type="submit">login</button>
+                </div>
+                </form>
+
+                <form class="signup-form">
+                <div>
+                    <label for="username-signup">username:</label>
+                    <input type="text" id="username-signup" />
+                </div>
+                <div>
+                    <label for="email-signup">email:</label>
+                    <input type="text" id="email-signup" />
+                </div>
+                <div>
+                    <label for="password-signup">password:</label>
+                    <input type="password" id="password-signup" />
+                </div>
+                <div>
+                    <button type="submit">signup</button>
+                </div>
+            </form>
             <div>
-                <h1>Login</h1>
                 <Link to="/Home">
                   Log In
                 </Link>
