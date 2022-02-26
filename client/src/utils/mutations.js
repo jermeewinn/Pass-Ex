b87af8afb8d1134c264905995e93587e6ca017ac
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-25 23:43:37 -0800

    sanity check

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,2 +1,13 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql``;
+
+export const ADD_USER = gql``;
+
+export const ADD_POST = gql``;
+
+export const DELETE_POST = gql``;
+
+export const ADD_COMMENT = gql``;
+
+export const DELETE_COMMENT = gql``;
