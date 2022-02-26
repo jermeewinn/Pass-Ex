afc354be0ebfd6435cfbc5f1ff970857de13a2c7
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-25 16:02:00 -0800

    defined preliminary typeDefs for server directory

--- Server/schemas/typeDefs.js
+++ Server/schemas/typeDefs.js
@@ -1,7 +1,48 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        posts: [POST]
+    }
 
+    type Post {
+        _id: ID
+        postText: String
+        createdAt: String
+        username: String
+        reactionCount: Int
+        comments: [Comment]
+    }
+
+    type Comment {
+        _id: ID
+        commentBody: String
+        createdAt: String
+        username: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+        posts(username: String): [Post]
+        post(_id: ID!): Post
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!) Auth
+        addPost(postText: String!): Post
+        addComment(postId: ID!, commentBody: String!): Post
+    }
 `;
 
-modeul.exports = typeDefs;
+module.exports = typeDefs;
