ca32ed85a1d644b141b64d3fd57e3d2a9f00f9c9
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-25 22:16:14 -0800

    imported apollo/client and built preliminary definition for GET_USER query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,27 @@
 import { gql } from '@apollo/client';
 
-export const 
+export const GET_USER = gql`
+    {
+        query user($usesrname: String!) {
+            user(username: $username) {
+                _id
+                username
+                email
+                posts {
+                    _id
+                    postText
+                    createdAt
+                    username
+                    commentCount
+                    comments: {
+                        _id
+                        commentBody
+                        createdAt
+                        username
+                    }
+                }
+            }
+        }
+    }
+`;
+
