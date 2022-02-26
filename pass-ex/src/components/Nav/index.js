fa804c88743f2b7a162182f6f146e5de920439f8
Kaylin Boyle <kaylinnboyle@gmail.com>
2022-02-25 23:31:47 -0800

    css styings, nav, and login

--- pass-ex/src/components/Nav/index.js
+++ pass-ex/src/components/Nav/index.js
@@ -3,30 +3,60 @@ import { Link } from "react-router-dom";
 
 function Nav() {
     return (
-        <section id="Nav">
-        <h2>
-          <a href="/">
-              Pass X
-          </a>
-        </h2>
-        <header className="flex-row px-1">
-          <nav>
-            <ul className="flex-row">
-              <li className="mx-2">
-                  
-                <Link to="/">
-                  Log Out
-                </Link>
-              </li>
-              <li className="mx-2">
-                  
-                <Link to="/YourAccount">
-                  Your Account
-                </Link>
-              </li>
+        <section>        
+          <section class="candy-stripe1">
+          <ul>
+            <li class="stripe"></li>
+            <li class="stripe"></li>
+            <li class="stripe"></li>
+            <li class="stripe"></li>
+            <li class="stripe"></li>
+            <li class="stripe"></li>
+            <li class="stripe"></li>
+            <li class="stripe"></li>
+            <li class="stripe"></li>
+            <li class="stripe"></li>
+          </ul>
+          </section>
+          <section id="Nav">
+          <div id="title">
+            <a id="title-words" href="/">
+                Pass X
+            </a>
+            <p>“Travel is the only thing you buy that makes you richer”<br></br> – Anonymous</p>
+            <img id="plane" src="https://www.kindpng.com/picc/m/5-57688_plane-png-image-airplane-clip-art-transparent-background.png" className="image" alt="plane logo" />
+          </div>
+          <header className="flex-row">
+            <nav>
+              <ul className="flex-row">
+                <li className="mx-2">
+                  <Link className="button is-info" to="/YourAccount">
+                    Your Account
+                  </Link> 
+                </li>
+                <li className="mx-2">
+                    <Link className="button is-info"  to="/">
+                    Log Out
+                    </Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          </section>
+          <section class="candy-stripe">
+            <ul>
+              <li class="stripe"></li>
+              <li class="stripe"></li>
+              <li class="stripe"></li>
+              <li class="stripe"></li>
+              <li class="stripe"></li>
+              <li class="stripe"></li>
+              <li class="stripe"></li>
+              <li class="stripe"></li>
+              <li class="stripe"></li>
+              <li class="stripe"></li>
             </ul>
-          </nav>
-        </header>
+          </section>
       </section>
     );
   }
