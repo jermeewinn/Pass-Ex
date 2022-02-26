966cfd35ff9d7e543aac417bbd52e4440f98bec1
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-25 16:29:40 -0800

    initial setup for resolvers.js

--- Server/schemas/resolvers.js
+++ Server/schemas/resolvers.js
@@ -3,7 +3,30 @@ const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('post')
+                return userData;
+            }
 
+            throw new AuthenticationError('Not logged in');
+        },
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('posts');
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('posts')
+        },
+        posts: async (parent)
+    },
+    
 };
 
 module.exports = resolvers;
