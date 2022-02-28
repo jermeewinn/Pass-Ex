6b63aa1ceae7c8bca8f4a5230dddba7da686c01d
Andrewy2416 <andrewy2416@gmail.com>
2022-02-28 01:07:17 -0800

    fixed api not loading

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pass-ex', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: true,
+  //useCreateIndex: true,
+  //useFindAndModify: true,
 });
 
 module.exports = mongoose.connection;
